456ea9199f01f8701f214ea7f287584f7932800b
Tilda Yngman <Matilda.yngman@hyperisland.se>
2021-12-17 15:15:42 +0100

    made som namechanges

--- script/script.js
+++ script/script.js
@@ -11,10 +11,13 @@ let dateAndTimeToString;
 //If the API desides to change the way they display the date and time stamp it will break and the weather will default to the error message inside let todaysWeather
 //Since hours only display single digit if < 10 i needed to append a 0 if it is a single digit to be able to compare.
 
-function getTodaysDate() {
+function getTodaysDateAndTime() {
     date = new Date();
     todaysDate = date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate()+'T';
     time = date.getHours();
+}
+
+function redefineDateAndTime() {
     if (time < 10) {
         time = "0" + date.getHours()
         todaysTimeAndDate = todaysDate + time;
@@ -47,7 +50,7 @@ function getWeatherData(data) {
     }
 };
 
-function getWeatherIcon(iconData) {
+function getIconFromLocalJson(iconData) {
     for (let i = 0; i < iconData.length; i++) {
         if (iconData[i].id === todaysWeatherNumber) {
             todaysWeatherSymbol = iconData[i].image;
@@ -57,20 +60,20 @@ function getWeatherIcon(iconData) {
     }
 }
 
-async function fetchWeatherImage() {
+async function fetchLocalJson() {
     let response = await fetch('../data/icon.json');
     let iconData = await response.json();
-    getWeatherIcon(iconData);
+    getIconFromLocalJson(iconData);
 };
 
-async function fetchWeatherApi() {
+async function fetchApi() {
     let response = await fetch('https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/12.2523/lat/62.5590/data.json');
     let data = await response.json();
-    getTodaysDate();
+    getTodaysDateAndTime();
+    redefineDateAndTime();
     getWeatherData(data);
-    await fetchWeatherImage();
+    await fetchLocalJson();
     displayWeather();
-    console.log(data)
 };
 
 function displayWeather() {
@@ -81,4 +84,4 @@ function displayWeather() {
     <h2 class="todaysWeather__number">${todaysWeather}&#8451</h2>`;
 };
 
-fetchWeatherApi();
+fetchApi();
